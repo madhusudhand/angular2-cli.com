76c9374b66f5f5f1aae497c448172d58928b4a47
madhusudhand <madhusudhan.dollu@gmail.com>
2016-05-30 12:13:19 +0530

    tslint

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,6 +1,6 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { AppComponent } from './shared/app.component/app.component';
+import { AppComponent } from './shared/app.component';
 import { environment } from './environment';
 
 if (environment.production) {
